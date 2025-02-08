d7f91852712cab4f42830cbdfb341556f5f1185a
MMBaplha <mardocheebruno5@gmail.com>
2025-02-07 19:58:59 -0500

    updated server.js file

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
 /** Start server for Lunchly. */
-const express = require("express");
-const app = express();
-
-const routes = require("./routes");
-
-// Middleware to parse JSON
-app.use(express.json());
-
-app.use("/", routes);
+const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
